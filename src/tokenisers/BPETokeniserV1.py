cb9dd51479d6f5248e9c4a36fa0fd51cd52e3b11
Kailas-Thonnithodi <kthonnithodi@gmail.com>
2026-02-21 17:45:03 +1100

    better understanding of sliding windows, DataSet and Dataloader iterable wrappers

--- src/tokenisers/BPETokeniserV1.py
+++ src/tokenisers/BPETokeniserV1.py
@@ -1,10 +1,71 @@
 import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
 
-'''
-Has basically a similar structure to SimpleTokeniserV2, however implements the BPE algorithm for deal with unknown values and certain string wrappers and separators.
-'''
+class GPTDatasetV1(Dataset):
+    
+    '''
+    Creating the sliding window dataset which contains the input and respective target lists. 
+    '''
+
+    def __init__(self, text, tokeniser, window_size, stride):
+        
+        self.input_ids = []
+        self.target_ids = []
+        token_ids = tokeniser.encode(text)
+
+        for idx in range(0, len(token_ids) - window_size, stride):
+            # check the image in the sliding window for saving inputs and targets in words document. 
+            input_chunk = token_ids[idx:idx + window_size]
+            target_chunk = token_ids[idx + 1:idx + window_size + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+    
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+def load_data(text: str, batch_size:int = 4, window_size: int = 256, stride: int = 128, shuffle: bool = False, drop_lost: bool = True, num_of_workers: int = 0):
+    '''
+    Creating an interative wrapper for the DataSet class (derived for our data.)
+    '''
+    tokeniser = tiktoken.get_encoding('o200k_base')
+    dataset = GPTDatasetV1(text, tokeniser, window_size, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size = batch_size,
+        shuffle = shuffle,
+        drop_last = drop_lost,
+        num_workers = num_of_workers
+    )
+    return dataloader
+
+def sliding_window(data: str, tiktokeniser: tiktoken.Encoding, window_size: int):
+    for idx in range(1, window_size+1):
+        window = data[:idx]
+        desired = data[idx]
+        print(f'[{tiktokeniser.decode(window)}]: {tiktokeniser.decode([desired])}')
 
 if __name__ == "__main__":
-    tiktokenisor = tiktoken.get_encoding("o200k_base")
-    integers = tiktokenisor.encode("<|unk|>", allowed_special={})
-    print(integers)
+    
+    file_dir = 'data/romeo_and_juliet.txt'
+
+    with open(file_dir, 'r', encoding='utf-8') as raw_file:
+        raw_text = raw_file.read()
+
+    # # Testing sliding window
+    # tiktokenisor = tiktoken.get_encoding("o200k_base")
+    # encoded_values = tiktokenisor.encode(raw_text)
+    # sample_encoded_values = encoded_values[20:80]
+    # context_size = 10
+    # sliding_window(sample_encoded_values, tiktokenisor, context_size
+
+    load_data = load_data(raw_text, batch_size=2, window_size=8, stride=3)
+    iter_load_data = iter(load_data)
+    print(f'First Iteration: {next(iter_load_data)}') # contains 2 tensors; 1st is the window with context, and second is the target_ids
+    print(f'Second Iteration: {next(iter_load_data)}')
+    print(f'Third Iteration: {next(iter_load_data)[0]}')
+
+
